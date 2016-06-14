c4836ab52063deec5b271956cb2f2733944eec37
Getty314 <tjg032001@gmail.com>
2016-06-14 15:26:34 -0500

    Add files via upload

---
+++ genericDrug.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import wikipedia
+
+actualDrug = input("Enter the non-generic name of the drug: ")
+
+wikipedia.search(actualDrug)
+
+GD = wikipedia.page(actualDrug)
+
+print("Generic Name:" + GD.title)
